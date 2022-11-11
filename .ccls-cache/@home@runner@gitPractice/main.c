1b465d9a05b10c57d3a34901b27cef3ff5327ed2
Dale Reed <dalereed@gmail.com>
2022-11-11 00:35:46 +0000

    Added a second bumper sticker.

---
+++ .ccls-cache/@home@runner@gitPractice/main.c
@@ -0,0 +1,31 @@
+// gitDemo.c
+//    Program to practice using git and gitHub
+/* Running the program looks like:
+        Bumper Stickers: 
+            Honk if you love peace and quiet
+        Sayings: 
+            Rats live on no evil star
+ */
+#include <stdio.h>
+
+void showBumperStickers()
+{
+    printf("\t Honk if you love peace and quiet\n");
+    printf("\t All generalizations are false.\n");
+}
+
+void showSayings()
+{
+     printf("\t Rats live on no evil star\n");   
+}
+
+
+int main(void) 
+{
+    printf("Bumper Stickers: \n");
+    showBumperStickers();
+    printf("Sayings: \n");
+    showSayings();
+    
+  return 0;
+}
